1f3fdbab5f394ee08482f97f8d4aad7e61b385d0
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2026-06-10 20:52:41 -0300

    Aula 15 8

--- src/main/java/com/algaworks/algashop/ordering/application/utility/SortablePageFilter.java
+++ src/main/java/com/algaworks/algashop/ordering/application/utility/SortablePageFilter.java
@@ -1,4 +1,17 @@
 package com.algaworks.algashop.ordering.application.utility;
 
-public abstract class SortablePageFilter {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.domain.Sort;
+
+import javax.swing.*;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public abstract class SortablePageFilter<T> extends PageFilter{
+    private T sortedByProperty;
+    private Sort.Direction direction;
+
+    public abstract T getSortByPropertyOrDefault();
+    public abstract Sort.Direction getSortDirectionOrDefault();
 }
