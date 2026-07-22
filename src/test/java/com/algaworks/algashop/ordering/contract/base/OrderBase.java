5a967c8adf44006c29c5b03772806c1097813105
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2026-07-21 22:38:02 -0300

    Capitulo 5

---
+++ src/test/java/com/algaworks/algashop/ordering/contract/base/OrderBase.java
@@ -0,0 +1,46 @@
+package com.algaworks.algashop.ordering.contract.base;
+
+import com.algaworks.algashop.ordering.application.order.query.OrderDetailOutputTestDataBuilder;
+import com.algaworks.algashop.ordering.application.order.query.OrderQueryService;
+import com.algaworks.algashop.ordering.domain.model.order.OrderNotFoundException;
+import com.algaworks.algashop.ordering.presentation.OrderController;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = OrderController.class)
+class OrderBase {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockitoBean
+    private OrderQueryService orderQueryService;
+
+    public static final String notFoundOrderId = "01226N0693HDH";
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(
+                MockMvcBuilders.webAppContextSetup(context)
+                        .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .build()
+        );
+        String validOrderId = "01226N0640J7Q";
+        RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
+        when(orderQueryService.findById(validOrderId))
+                .thenReturn(OrderDetailOutputTestDataBuilder.placedOrder(validOrderId).build());
+
+        when(orderQueryService.findById(notFoundOrderId))
+                .thenThrow(new OrderNotFoundException());
+    }
+}
